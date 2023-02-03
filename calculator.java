7dad7a5ea657122db504066cdf83117bfaa5647f
NAWAZ-SRM <115914519+NAWAZ-SRM@users.noreply.github.com>
2023-02-03 19:55:18 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Calculator {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter first number: ");
+    double num1 = input.nextDouble();
+
+    System.out.print("Enter second number: ");
+    double num2 = input.nextDouble();
+
+    System.out.print("Enter an operator (+, -, *, /): ");
+    char operator = input.next().charAt(0);
+
+    double result;
+
+    switch(operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+
+      case '-':
+        result = num1 - num2;
+        break;
+
+      case '*':
+        result = num1 * num2;
+        break;
+
+      case '/':
+        result = num1 / num2;
+        break;
+
+      default:
+        System.out.printf("Error! Operator is not correct");
+        return;
+    }
+
+    System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+  }
+}
